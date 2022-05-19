af4988882be1d1e3d0750e48d51d6aeb8bf45fa2
Silver <testingbugsrus@gmail.com>
2022-05-19 12:09:57 +0300

    Add Configuration.remote

--- src/test/java/com/demoqa/formTests/TestSetup.java
+++ src/test/java/com/demoqa/formTests/TestSetup.java
@@ -12,6 +12,7 @@ public class TestSetup {
     static void setup() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
